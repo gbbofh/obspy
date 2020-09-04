a8ae541adf8b5391b119db9f1c3e49b263f5f5a9
Garrett Bates <garrett.bates@alumni.nmt.edu>
2020-09-04 15:12:09 -0600

    Started working on methods to decompress various types of data

--- obspy/io/grf/libgrf.py
+++ obspy/io/grf/libgrf.py
@@ -24,42 +24,74 @@ def read_header(fp):
     """
     header = fp.read(13)
     hdict = {
-    "magic": header[0:3],
-    "version": header[3:4], # 1 byte
-    "size": header[4:6],    # 2 bytes
-    "seq": header[6:8],     # 2 bytes
-    "unit": header[8:12],   # 4 bytes
-    "type": header[12:14],  # 1 byte
+        "magic": header[0:3],
+        "version": header[3:4], # 1 byte
+        "size": header[4:6],    # 2 bytes
+        "seq": header[6:8],     # 2 bytes
+        "unit": header[8:12],   # 4 bytes
+        "type": header[12:14],  # 1 byte
     }
 
     return hdict
 
 
-def read_data_packet_header(fp):
-    header = fp.read(76)
+def decode_waveform_int32(waveform):
+    pass
+
+
+def decode_waveform_int24(waveform):
+    pass
+
+
+def decode_waveform_cm8(waveform):
+    pass
+
+
+def parse_data_packet(packet):
     # Double check these offsets against the spec
     hd = {
-    "channel_number": header[0:2],
-    "network": header[2:10],
-    "station": header[10:26],
-    "component": header[26:34],
-    "time_quality": header[34:35],
-    "rate_quality": header[35:36],
-    "start_time": header[36:44],
-    "time_correction": header[44:52],
-    "sample_rate": header[52:60],
-    "sample_correction"]: header[60:68],
-    "cpv": header[68:72],
-    "datatype": header[72:73],
-    "num_samples": header[73:77],
+        "channel_number": header[0:2],
+        "network": header[2:10],
+        "station": header[10:26],
+        "component": header[26:34],
+        "time_quality": header[34:35],
+        "rate_quality": header[35:36],
+        "start_time": header[36:44],
+        "time_correction": header[44:52],
+        "sample_rate": header[52:60],
+        "sample_correction"]: header[60:68],
+        "cpv": header[68:72],
+        "datatype": header[72:73],
+        "num_samples": header[73:75],
     }
+
+    decoders = {
+        0: decode_waveform_int32,
+        1: decode_waveform_int24,
+        2: decode_waveform_cm8,
+    }
+
+    dtype = int.from_bytes(hd["datatype"], sys.byteorder)
+    # TODO: Restructure to make clearer -- use exception handling
+    if dtype in decoders:
+        data = decoders[dtype](packet[75:])
+
+    return hd, data
 
 
 def read_packet(fp):
+    handlers = {
+        1: parse_data_packet,
+    }
+
     header = read_header(fp)
     packet_data = fp.read(header["size"] - 13)
 
-    return (header, packet_data)
+    packet_type = int.from_bytes(header["type"], sys.byteorder, signed = False)
+    if packet_type in handlers:
+        packet_header, data = handlers[packet_type](packet_data)
+
+    return (header, packet_header, data)
 
 
 def read_grf(fp):
